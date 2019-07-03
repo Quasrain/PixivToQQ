4534254528912d562e36d6c36b484ec24f25441f
1171000622 <lanceriet@gmail.com>
2019-07-03 22:52:37 +0800

    1.0.1 each picture forms a message

--- Quasrain/plugins/pixiv.py
+++ Quasrain/plugins/pixiv.py
@@ -9,11 +9,12 @@ from urllib.request import urlopen
 @on_command('pixiv', aliases=('pictures'))
 async def pixiv(session: CommandSession):
     # 从会话状态（session.state）中获取城市名称（city），如果当前不存在，则询问用户
-    city = session.get('tag', prompt='你想查询哪个城市的天气呢？')
+    tag = session.get('tag', prompt='你想看哪个老婆呢？')
     # 获取城市的天气预报
-    weather_report = await get_url_of_tag(city)
+    for i in range(4):
+        pictures = await get_url_of_tag(tag,i)
     # 向用户发送天气预报
-    await session.send(weather_report)
+        await session.send(pictures)
 
 
 # weather.args_parser 装饰器将函数声明为 weather 命令的参数解析器
@@ -34,12 +35,12 @@ async def _(session: CommandSession):
     if not stripped_arg:
         # 用户没有发送有效的城市名称（而是发送了空白字符），则提示重新输入
         # 这里 session.pause() 将会发送消息并暂停当前会话（该行后面的代码不会被运行）
-        session.pause('要查询的城市名称不能为空呢，请重新输入')
+        session.pause('要查询的老婆名称不能为空呢，请重新输入')
 
     # 如果当前正在向用户询问更多信息（例如本例中的要查询的城市），且用户输入有效，则放入会话状态
     session.state[session.current_key] = stripped_arg
 
-async def get_url_of_tag(tag: str) -> str:
+async def get_url_of_tag(tag: str,k) -> str:
     # 这里简单返回一个字符串
     # 实际应用中，这里应该调用返回真实数据的天气 API，并拼接成天气预报内容
     url = 'http://pixiv.navirank.com/tag/'+tag
@@ -59,9 +60,9 @@ async def get_url_of_tag(tag: str) -> str:
         filename = Res[1]+'.jpg'
         # print(trueurl+Res[0]+'\n')
         print(trueurl+urls+'\n')
-        ans = ans+"[CQ:image,file="+trueurl+urls+"]"+'\n'
+        ans = "[CQ:image,file="+trueurl+urls+"]"
         # ans = ans+"[CQ:image,file="+'1.jpg'+"]"
         # ans = ans + trueurl + urls+ '\n';
-        if (cnt == 10):
+        if (cnt > k):
             break
     return ans
